14c8a61a6925859aaae1ce63a1628ee787a1b95f
Caleb Connolly <caleb@connolly.tech>
2021-10-21 21:44:48 +0100

    First commit, it WORKS!!!
    For parsing smb2 log dumps this is pretty much complete.

---
+++ README.md
@@ -0,0 +1,87 @@
+# Where's That Define
+
+WTD is a python tool for replacing values with the C preprocessor macro which defined them. An example of this is when trying to understand a driver by dumping every read() and write(), you end up with a big list of values like the following:
+
+```
+[  162.326260] qcom,qpnp-smb2: smblib_read(addr = 0x1307, val=0x01)
+[  162.326289] qcom,qpnp-smb2: smblib_read(addr = 0x1308, val=0x08)
+[  162.326373] qcom,qpnp-smb2: smblib_read(addr = 0x1307, val=0x01)
+[  162.326539] qcom,qpnp-smb2: smblib_read(addr = 0x1070, val=0x79)
+[  162.326582] qcom,qpnp-smb2: smblib_read(addr = 0x1061, val=0x4e)
+[  162.326623] qcom,qpnp-smb2: smblib_read(addr = 0x1607, val=0x13)
+[  162.326666] qcom,qpnp-smb2: smblib_read(addr = 0x1007, val=0x80)
+[  162.326702] qcom,qpnp-smb2: smblib_read(addr = 0x1365, val=0xf7)
+```
+
+Wouldn't it be so much better if it could look like this instead?
+```
+[ 7291.659328] qcom,qpnp-smb2: smblib_read(addr = APSD_STATUS_REG, val = APSD_DTC_STATUS_DONE_BIT)
+[ 7291.659358] qcom,qpnp-smb2: smblib_read(addr = APSD_RESULT_STATUS_REG, val = DCP_CHARGER_BIT|APSD_RESULT_STATUS_MASK=0x8)
+[ 7291.659457] qcom,qpnp-smb2: smblib_read(addr = APSD_STATUS_REG, val = APSD_DTC_STATUS_DONE_BIT)
+[ 7291.659610] qcom,qpnp-smb2: smblib_read(addr = FLOAT_VOLTAGE_CFG_REG, val = FLOAT_VOLTAGE_SETTING_MASK=0x79)
+[ 7291.659652] qcom,qpnp-smb2: smblib_read(addr = FAST_CHARGE_CURRENT_CFG_REG, val = FAST_CHARGE_CURRENT_SETTING_MASK=0x4e)
+[ 7291.659694] qcom,qpnp-smb2: smblib_read(addr = ICL_STATUS_REG, val = INPUT_CURRENT_LIMIT_MASK=0x13)
+[ 7291.659739] qcom,qpnp-smb2: smblib_read(addr = BATTERY_CHARGER_STATUS_2_REG, val = 0x00)
+[ 7291.659776] qcom,qpnp-smb2: smblib_read(addr = USBIN_LOAD_CFG_REG, val = USBIN_OV_CH_LOAD_OPTION_BIT|ICL_OVERRIDE_AFTER_APSD_BIT)
+[ 7291.659807] qcom,qpnp-smb2: smblib_read(addr = USBIN_CURRENT_LIMIT_CFG_REG, val = USBIN_CURRENT_LIMIT_MASK=0x3c)
+[ 7291.661133] qcom,qpnp-smb2: smblib_read(addr = USB_INT_RT_STS, val = USBIN_PLUGIN_RT_STS_BIT)
+```
+
+Well with the power of WTD and Regex, it can!
+
+Yes that's right, this is a header file parser using regex.
+
+## Usage
+
+Before using WTD, some manual processing of `#define`s is required, WTD can parse macros in the following formats:
+
+```c
+// The literal 3735941133 in hex
+#define SOME_MACRO 0xdeadf00d
+```
+```c
+// The literal 1234
+#define SOME_MACRO 1234
+```
+```c
+// Bit 2
+#define SOME_MACRO BIT(2)
+```
+```c
+// Bitmask from bit 2 to bit 0
+#define SOME_MACRO GENMASK(2, 0)
+```
+
+The header parser uses context to relate a bunch of bit / mask macros to a particular register, as such the ordering matters. The symbol tree will attribute all BIT() and GENMASK() macros to whatever the last literal value register was.
+For example:
+```h
+#define BATTERY_CHARGER_STATUS_5_REG			0x100B
+#define VALID_INPUT_POWER_SOURCE_BIT			BIT(7)
+#define DISABLE_CHARGING_BIT				BIT(6)
+#define FORCE_ZERO_CHARGE_CURRENT_BIT			BIT(5)
+#define CHARGING_ENABLE_BIT				BIT(4)
+#define TAPER_BIT					BIT(3)
+#define ENABLE_CHG_SENSORS_BIT				BIT(2)
+#define ENABLE_TAPER_SENSOR_BIT				BIT(1)
+#define TAPER_REGION_BIT				BIT(0)
+```
+produces the following tree:
+
+```py
+BATTERY_CHARGER_STATUS_5_REG: {
+    'value': 4107,
+    'bits':
+    [
+        {'name': 'VALID_INPUT_POWER_SOURCE_BIT', 'bit': 7},
+        {'name': 'DISABLE_CHARGING_BIT', 'bit': 6},
+        {'name': 'FORCE_ZERO_CHARGE_CURRENT_BIT', 'bit': 5},
+        {'name': 'CHARGING_ENABLE_BIT', 'bit': 4},
+        {'name': 'TAPER_BIT', 'bit': 3},
+        {'name': 'ENABLE_CHG_SENSORS_BIT', 'bit': 2},
+        {'name': 'ENABLE_TAPER_SENSOR_BIT', 'bit': 1},
+        {'name': 'TAPER_REGION_BIT', 'bit': 0}
+    ],
+    'masks': []}
+```
+
+Some semi-manual preprocessing may be required before using this tool, personally I've found the multi-cursor feature in vscode to be very useful, as well as regex-based find and replace. You could also do this with sed / grep or whatever your preferred method is.
---
+++ wtd.py
@@ -0,0 +1,119 @@
+#!/usr/bin/python3
+
+import re, argparse
+
+def parse_args():
+     parser = argparse.ArgumentParser(description='Convert logs which print macro values to the macro names')
+     parser.add_argument('logfile',
+                         help='Path to logfile')
+     parser.add_argument('header',
+                         help='path to the header file containing #defines.'
+                              'Supported defines include literals and BIT()s.')
+     parser.add_argument('outfile',
+                         help='path to processed output file')
+     try:
+          return parser.parse_args()
+     except argparse.ArgumentError:
+          parser.print_help()
+          return None
+
+def num_to_int(num: str):
+     if "x" in num:
+          return int(num, 16)
+     else:
+          return int(num)
+
+def genmask(left: int, right: int):
+     if right > left:
+          right, left = left, right
+     val = 0
+     for x in range(0, left-right + 1):
+          val |= 1 << x
+     return val << right
+
+def genmask_mask(mask: tuple):
+     return genmask(mask[0], mask[1])
+
+def map_header(header_lines):
+     header_map = {}
+     """
+     The part of the comment in quotes is what we are trying to match
+     """
+                                        # #define "blah"
+     match_name = re.compile("(?<=#define\s)[a-zA-Z\d_]+(?=\s)", re.MULTILINE)
+                                        # #define blah "0x123"
+     match_value = re.compile("((0[xX][a-fA-F\d]+)|\d+)$", re.MULTILINE)
+                                        #define blah BIT("3")
+     match_value_bit = re.compile("(?<=\sBIT\()((0[xX][[a-fA-F\d]])|\d+)", re.MULTILINE)
+                                        #define blah GENMASK("2", "0") <-- matches two groups
+     match_value_mask = re.compile("(?<=\sGENMASK\()(\d),[\s](\d)", re.MULTILINE)
+     for line in header_lines:
+          name = match_name.search(line)
+          value = match_value.search(line)
+          bit = match_value_bit.search(line)
+          mask = match_value_mask.search(line)
+          if name:
+               if value:
+                    val = num_to_int(value.group(0))
+                    header_map[name.group(0)] = {"value": val, "bits": [], "masks": []}
+               elif bit:
+                    header_map[list(header_map.keys())[-1]]["bits"].append({"name": name.group(0), "bit": int(bit.group(0))})
+               elif mask:
+                    header_map[list(header_map.keys())[-1]]["masks"].append({"name": name.group(0), "mask": (int(mask.group(1)), int(mask.group(2)))})
+     return header_map
+
+def convert_val(val, register_data):
+     out_str = ""
+     for bit in register_data["bits"]:
+          if val & (1 << bit["bit"]):
+               out_str += bit["name"] + "|"
+     for mask in register_data["masks"]:
+          if val & genmask_mask(mask["mask"]):
+               out_str += mask["name"] + "=" \
+               + hex(val & genmask_mask(mask["mask"]) >> mask["mask"][1]) + "|"
+     return out_str[:-1]
+
+def process_logs(logfile_lines, header):
+     re_hex = "(0[xX][a-fA-F\d]+)"
+     match_addr = re.compile(f"(addr\s=\s){re_hex}", re.MULTILINE)
+     match_val = re.compile(f"(val\s=\s){re_hex}", re.MULTILINE)
+     #[ 7292.713722] qcom,qpnp-smb2 c440000.qcom,spmi:qcom,pmi8998@2:qcom,qpnp-smb2: smblib_read(addr = 0x100d, val = 0x70)
+
+     for i in range(len(logfile_lines)):
+          line = logfile_lines[i]
+          addr = match_addr.search(line)
+          val = match_val.search(line)
+          if not addr or not val:
+               continue
+          addr = addr.group(2)
+          val = val.group(2)
+          #print(f"{addr} = {val}")
+          addr_int = num_to_int(addr)
+          val_int = num_to_int(val)
+          for key in header.keys():
+               if header[key]["value"] == addr_int:
+                    logfile_lines[i] = logfile_lines[i].replace(f"addr = {addr}", f"addr = {key}")
+                    val_str = convert_val(val_int, header[key])
+                    if val_str:
+                         logfile_lines[i] = logfile_lines[i].replace(f"val = {val}", f"val = {val_str}")
+               # elif header[key]["value"] == val_int:
+               #      logfile_lines[i] = logfile_lines[i].replace(f"val = {val}", f"val = {key}")
+     return logfile_lines
+
+
+def main():
+     args = parse_args()
+     logfile_f = open(args.logfile, "r")
+     logfile = [x.strip() for x in logfile_f.readlines()]
+     header_f = open(args.header, "r")
+     header = map_header([x.strip() for x in header_f.readlines()])
+     for key in list(header.keys()):
+          print(f"{key}: {header[key]}")
+     fixed_log = process_logs(logfile, header)
+     f = open(args.outfile, "w")
+     f.write("\n".join(fixed_log))
+     f.flush()
+     f.close()
+
+if __name__ == "__main__":
+     main()
